32e4d387a11496a7da55768cc0eee2a5a6b36c24
Arafat10 <arafat.nabil.10@gmail.com>
2023-12-25 21:02:57 +0600

    recipe api added

---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:food_recipe/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.http(
+        'yummly2.p.rapidapi.com', '/feeds/list', {'limit': '24', 'start': '0'});
+
+    final response = await http.get(uri, headers: {
+      'X-RapidAPI-Key': '9a917d05a7msh3b114f479f9ea47p19735djsnad3a73388d95',
+      'X-RapidAPI-Host': 'yummly2.p.rapidapi.com',
+      'useQueryString': 'true'
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
