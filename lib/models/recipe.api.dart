5a77c35e1aee4b843e92275369f1eed59a68491a
Arafat10 <35999865+Arafat10@users.noreply.github.com>
2023-12-25 22:55:57 +0600

    Update recipe.api.dart
    final touches(1)

--- lib/models/recipe.api.dart
+++ lib/models/recipe.api.dart
@@ -4,12 +4,10 @@ import 'package:http/http.dart' as http;
 
 class RecipeApi {
   static Future<List<Recipe>> getRecipe() async {
-    var uri = Uri.http('http://yummly2.p.rapidapi.com', '/feeds/list',
-        {'limit': '24', 'start': '0'});
+    var uri = Uri.http();
 
     final response = await http.get(uri, headers: {
-      'X-RapidAPI-Key': '9a917d05a7msh3b114f479f9ea47p19735djsnad3a73388d95',
-      'X-RapidAPI-Host': 'http://yummly2.p.rapidapi.com',
+      '
     });
 
     Map data = jsonDecode(response.body);
